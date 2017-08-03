cd39d8cea30a7566ab792265c0cd748218f83169
Zhanwen xu <leon.xu@kii.com>
2017-08-03 18:07:15 +0800

    order confirm

--- src/screens/shops/OrderConfirm.js
+++ src/screens/shops/OrderConfirm.js
@@ -2,7 +2,7 @@
  * Created by leonardean on 03/08/2017.
  */
 import React, {Component} from 'react';
-import {Text, View, StyleSheet, TouchableOpacity, ScrollView} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity, ScrollView, ActivityIndicator, FlatList, Alert} from 'react-native';
 import Picker from 'react-native-picker';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Global from '../../Global';
@@ -11,7 +11,10 @@ export default class OrderConfirm extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            order: Object.assign({}, this.props.order, { payment: 'Credit Point' }),
+            order: Object.assign({}, this.props.order, {
+                payment: 'Credit Point',
+                total_price: this.props.order.total_price + this.props.order.delivery_fee
+            }),
             ready: false,
             isLoading: true
         }
@@ -27,10 +30,16 @@ export default class OrderConfirm extends Component {
         })
             .then((response) => response.json())
             .then((responseJson) => {
+
                 const recipient = responseJson.recipients.map((recipient) => {
-                    if (recipient.primary === true)
+                    if (recipient.primary === true) {
+                        this.setState({
+                            ready: true
+                        })
                         return recipient
+                    }
                 })
+                console.log(recipient[0])
                 this.setState({
                     isLoading: false,
                     order: Object.assign({}, this.state.order, { customer: {
@@ -48,6 +57,10 @@ export default class OrderConfirm extends Component {
 
     componentWillUnmount() {
         Picker.hide()
+    }
+
+    placeOrder = () => {
+        Alert.alert('Order Placed !')
     }
 
     onPress = () => {
@@ -68,7 +81,7 @@ export default class OrderConfirm extends Component {
             pickerFontSize: 20,
             onPickerConfirm: data => {
                 this.setState({
-                    order: Object.assign({}, this.props.order, { payment: data[0]})
+                    order: Object.assign({}, this.state.order, { payment: data[0]})
                 })
             },
             onPickerCancel: data => {
@@ -81,15 +94,26 @@ export default class OrderConfirm extends Component {
     }
 
     render() {
+        if (this.state.isLoading) {
+            return (
+                <View style={{flex: 1, paddingTop: 20}}>
+                    <ActivityIndicator />
+                </View>
+            );
+        }
         return (
             <View style={styles.container}>
                 <View style={styles.contentContainer}>
                     <ScrollView>
                         <TouchableOpacity style={[styles.segment, styles.row]}>
                             <View>
-                                <Text style={styles.textMain}>Mr. Andy Williams</Text>
-                                <Text>18551815695</Text>
-                                <Text style={styles.textMain}>南方心存178号502室</Text>
+                                <Text style={styles.textMain}>
+                                    {this.state.order.customer.recipient.gender === 0 ? "Mr. " : "Ms. "}
+                                    {this.state.order.customer.recipient.first_name + " "}
+                                    {this.state.order.customer.recipient.family_name + "  "}
+                                </Text>
+                                <Text>{this.state.order.customer.recipient.phone}</Text>
+                                <Text style={styles.textMain}>{this.state.order.customer.recipient.place.formatted_address}</Text>
                             </View>
                             <View style={{justifyContent: 'center'}}>
                                 <Icon name="ios-arrow-forward" size={25} color="#a2a2a2"/>
@@ -104,11 +128,42 @@ export default class OrderConfirm extends Component {
                                 <Icon name="ios-arrow-forward" size={25} color="#a2a2a2"/>
                             </View>
                         </TouchableOpacity>
+                        <View style={styles.segment}>
+                            <View style={styles.row}>
+                                <Text style={styles.textMain}>{this.state.order.shop.name}</Text>
+                            </View>
+                            <View style={styles.row}>
+                                <FlatList
+                                    data={this.state.order.items.map((item, index) => {
+                                        return Object.assign({}, item, { key: index})
+                                    })}
+                                    renderItem={({item}) => { return (<View style={{flexDirection: 'row'}}>
+                                        <View style={{flex: 1}}>
+                                            <Text style={styles.textMinor} numberOfLines={2}>{item.name}</Text>
+                                        </View>
+                                        <View style={{width: 50}}>
+                                            <Text style={styles.textMinor} numberOfLines={1}>{item.quantity}</Text>
+                                        </View>
+                                        <View style={{width: 50, alignItems: 'flex-end'}}>
+                                            <Text style={styles.textMinor} numberOfLines={1}>$ {item.sub_total}</Text>
+                                        </View>
+                                    </View>)}}
+                                />
+                            </View>
+                            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                                <Text style={styles.textMinor}>Delivery Fee</Text>
+                                <Text style={styles.textMinor}>$ {this.state.order.delivery_fee}</Text>
+                            </View>
+                        </View>
                     </ScrollView>
                 </View>
 
                 <View style={styles.summaryContainer}>
-
+                    <View style={styles.summary}>
+                        <View style={styles.totalPrice}>
+                            <Text style={{fontSize: 16, color: '#0c64ff'}}>Total: $ {this.state.order.total_price} </Text>
+                        </View>
+                    </View>
                     <Icon.Button name="ios-flag" size={22} iconStyle={{marginRight: 10}}
                                  color="white"
                                  backgroundColor={!this.state.ready ? '#91b9ff' : '#0c64ff'}
@@ -149,9 +204,22 @@ const styles = StyleSheet.create({
         marginVertical: 10,
         color: '#a2a2a2'
     },
+    textMinor: {
+        marginVertical: 8,
+        color: '#a2a2a2',
+        fontSize: 12
+    },
     summaryContainer: {
         height: 40,
-        backgroundColor: 'green',
         flexDirection: 'row',
-    }
+    },
+    summary: {
+        flex: 1,
+        backgroundColor: 'white',
+        borderTopWidth: 0.5,
+        paddingLeft: 10,
+        borderTopColor: '#a2a2a2',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
 });
